7aa0b2f647a2888814d47cb82e3fb5304f7a950e
Juan David Marin <juan_da-mari@hotmail.com>
2023-02-10 09:01:53 -0500

    empezando hacer modal

--- lista de mercado/src/App/AppUI.jsx
+++ lista de mercado/src/App/AppUI.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TodoContext } from '../TodoContext'
 import { TodoCounter } from '../TodoCounter';
 import {TodoList} from '../TodoList'
@@ -8,39 +8,45 @@ import { TodoItem } from '../TodoItem'
 import { LoadingPage } from "../NotFound/NotFound";
 
 export function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+  } = useContext(TodoContext); //aca llama directamente los parametros en vez de poner 'value' a cada propiedad que lo necesite, por ser función
+              
     return (
       <section className='flex flex-col h-screen justify-center items-center bg-blue-700'>
         <div className='space-y-4 w-96 h-auto rounded-lg shadow-xl text-center border-solid border-2 border-sky-500 shadow-blue-500/50 bg-blue-500 '>
           <TodoCounter/>
           <TodoSearch/>
-          
-          <TodoContext.Consumer>
-            {({
-              error,
-              loading,
-              searchedTodos,
-              completeTodo,
-              deleteTodo,
-            }) => (
-              <TodoList>
-                {error && <p>Desespérate, hubo un error...</p>}
-                {loading && <LoadingPage/>}
-                {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
 
-                {searchedTodos.map(todo => (
-                  <TodoItem
-                    text={todo.text}
-                    key={todo.text}
-                    completed={todo.completed}
-                    onComplete={() => completeTodo(todo.text)}
-                    onDelete={() => deleteTodo(todo.text)}
-                  />
-                ))}
-              </TodoList>
-            ) }
-          </TodoContext.Consumer>
+          <TodoList>
+            {error && <p>Desespérate, hubo un error...</p>}
+            {loading && <LoadingPage />}
+            {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+
+            {searchedTodos.map(todo => (
+              <TodoItem
+                text={todo.text}
+                key={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
+
           <CreateTodoButton></CreateTodoButton>
         </div>
       </section>
     )
   }
+
+/*   const defaultTodos = [
+    { text: 'Cortar cebolla', completed: true },
+    { text: 'Tomar el cursso de intro a React', completed: false },
+    { text: 'Llorar con la llorona', completed: true },
+    { text: 'LALALALAA', completed: false },
+  ]; */
--- lista de mercado/src/TodoCounter/index.jsx
+++ lista de mercado/src/TodoCounter/index.jsx
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
+import { TodoContext } from "../TodoContext"
 
 
-export function TodoCounter ({total, completed}) {
+export function TodoCounter () { //en parametros usaba props, ahora con 'usecontext' los reutilizo
+    const {totalTodos, completedTodos} = useContext(TodoContext);
     return(
-        <h2 className='text-2xl font-bold text-fuchsia-700'>Has completado {completed} de {total} TODOs</h2>
+        <h2 className='text-2xl font-bold text-fuchsia-700'>Has completado {completedTodos} de {totalTodos} TODOs</h2>
     )
 }
 
