0d8739f8d72ec8332deaf42b2aa4bdc4fc58e297
Juan David Marin <juan_da-mari@hotmail.com>
2023-02-27 21:01:44 -0500

    deploy

--- lista de mercado/src/App/AppUI.jsx
+++ lista de mercado/src/App/AppUI.jsx
@@ -56,10 +56,3 @@ export function AppUI() {
       </section>
     )
   }
-
-/*   const defaultTodos = [
-    { text: 'Cortar cebolla', completed: true },
-    { text: 'Tomar el cursso de intro a React', completed: false },
-    { text: 'Llorar con la llorona', completed: true },
-    { text: 'LALALALAA', completed: false },
-  ]; */
--- lista de mercado/src/addNew/index.jsx
+++ lista de mercado/src/addNew/index.jsx
@@ -5,7 +5,14 @@ export function AddNewTodo (){
         <section className=' bg-blue-700 rounded-lg'>
             <div className='space-y-4 shadow-xl text-center shadow-blue-500/50 bg-blue-500 top-1'>
                 <img src="../../public/imagenes/icontarea.png" alt="imagen de tarea" className="w-[80px] inline-block text-center p-2"/> 
-                <h3>crea tus nuevas tareas pendietes!</h3>
+                
+                <div className="flex m-5 space-x-0.5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 bg-[#61dafa] rounded-full text-white">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg> 
+                <h3 className="">crea tus nuevas tareas pendietes!</h3>
+                </div>
+
             </div>
         </section>
     )
